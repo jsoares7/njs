c2c3cf70b917e66a39612ddd96e87ec363a71280
Joao Soares <j.soares@njsdigital.com>
2021-09-19 18:07:39 +0100

    Installed Tailwind UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
